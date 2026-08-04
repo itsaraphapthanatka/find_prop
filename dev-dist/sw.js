95b7f8bf05d86db0fda5217b9496b04346ac29a5
itsaraphap <itsaraphap.top@gmail.com>
2026-08-04 13:08:29 +0700

    fix: update revision number for index.html in service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-4a0c0fd0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.0rgnacjh4vk"
+    "revision": "0.m7fmodu7i0k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
